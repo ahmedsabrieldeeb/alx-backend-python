85d11e3d45edf1c8196b7a210e7aa22151cede3b
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 15:33:32 +0300

    Annotated type floor func to take the float of a float in 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function floor that returns the floor of a float
+"""
+
+
+def floor(n: float) -> float:
+    """
+    This function returns the floor of a float
+
+    Args:
+        n: float
+    
+    Returns:
+        The floor of n
+    """
+    return float(n)
