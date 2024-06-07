78c234c49c7387e7e2f788a2e1416ea236c59b62
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 16:22:21 +0300

    Update element_length function to include type annotations
    The `element_length` function in `9-element_length.py` has been updated to include type annotations. This improves code readability and maintainability.

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+element_length module
+"""
+from typing import List, Tuple, Sequence
+
+
+def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    This function takes a list of sequences and returns a list of tuples
+    where each tuple contains a sequence and its length.
+
+    Args:
+        lst (List[Sequence]): list of sequences
+
+    Returns:
+        List[Tuple[Sequence, int]]: list of tuples
+    """
+    return [(i, len(i)) for i in lst]
