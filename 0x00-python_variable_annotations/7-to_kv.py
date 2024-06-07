2324876c63f2d25b14da95b128f30ddfc36e55ea
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 16:03:51 +0300

    Update to_kv function to include type annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function sum_list
+that returns the sum of a list of floats and ints
+"""
+from typing import List, Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    This function returns the sum of a list of floats and ints
+
+    Args:
+        input_list: List[int, float]
+
+    Returns:
+        The sum of the list
+    """
+    return (k, v*v)
