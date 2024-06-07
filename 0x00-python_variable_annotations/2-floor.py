f15017c4d3503ad29b0c36852bc5650c67ae6107
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 15:35:33 +0300

    chore: Update floor function to return integer value

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,14 +4,15 @@ This module contains the function floor that returns the floor of a float
 """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     This function returns the floor of a float
 
     Args:
         n: float
-    
+
     Returns:
         The floor of n
     """
-    return float(n)
+    import math
+    return math.floor(n)
