761ab50f974af99a553e6a2827ed5d0206d40da9
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 15:37:46 +0300

    Update to_str function to include docstring and type annotations
    The `to_str` function in `3-to_str.py` has been updated to include a docstring and type annotations. This improves code readability and maintainability.

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function to_str that returns the floor of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function returns the floor of a float
+
+    Args:
+        n: float
+    
+    Returns:
+        The floor of n
+    """
+    return str(n)
