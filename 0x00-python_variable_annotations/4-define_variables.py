23e8dbc5976a5cfee7bc4ae9b30f14127cb4ee60
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 15:41:24 +0300

    Update variable annotations in 4-define_variables.py
    This commit updates the variable annotations in the file 4-define_variables.py. The variables `a`, `pi`, `i_understand_annotations`, and `school` now have explicit type annotations, improving code readability and maintainability.

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the variables:
+a, pi, i_understand_annotations, and school
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
