85508eb914c802cbfd4c64b40f8b14b61192a2e4
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 15:30:27 +0300

    Annotated type concat func to conactente two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function concat that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concatenates two strings
+
+    Args:
+        str1: string
+        str2: string
+
+    Returns:
+        The concatenation of str1 and str2
+    """
+    return str1 + str2
