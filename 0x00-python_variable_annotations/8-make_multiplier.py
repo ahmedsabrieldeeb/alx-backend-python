05567ac85d88f0dde9c7aeb966684345342c9372
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-07 16:13:13 +0300

    Add type annotations to the make_multiplier function in 8-make_multiplier.py. This improves code readability and maintainability.

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+make_multiplier module
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplir: float) -> Callable[[float], float]:
+    """
+    This function takes a float as argument and returns a function that
+    takes a float as argument
+    and returns the product of the argument of the two functions.
+
+    Args:
+        multiplir (float): float to multiply
+
+    Returns:
+        Callable[[float], float]: function that does the action
+    """
+    def multiply(x: float) -> float:
+        return x * multiplir
+
+    return multiply
