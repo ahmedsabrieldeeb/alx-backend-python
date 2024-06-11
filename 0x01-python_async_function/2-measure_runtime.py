5429a1110e251144328be518bc3dabb3d70fca42
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-11 04:09:26 +0300

    chore: Update shebang to use env for Python3 in measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""The basics of async"""
+
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the runtime
+
+    Args:
+        n (int): The number of times to call wait_random
+        max_delay (int): The maximum random delay
+
+    Returns:
+        avg (float): The runtime per call (avergae)
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    avg = (end - start) / n
+    return avg
