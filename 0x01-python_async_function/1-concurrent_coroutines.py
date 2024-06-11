926c9b5bfe7930728c1b07b71674089d498c6b21
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-11 03:21:31 +0300

    Update wait_n function to use asyncio.gather for concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""The basics of async"""
+
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Waits for a random delay between 0 and `max_delay` seconds and returns it
+
+    Args:
+        n (int): The number of times to call wait_random
+        max_delay (int): The maximum random delay
+
+    Returns:
+        ret (float): The list of all the delays
+    """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    ret = await asyncio.gather(*delays)
+    ret.sort()
+    return ret
