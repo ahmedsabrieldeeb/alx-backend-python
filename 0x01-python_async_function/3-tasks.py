5d13fe1d50548b6c4037569588d836a39d8c59ec
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-11 06:36:53 +0300

    create tasks to be scheduled

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Tasks """
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    Task wait random
+
+    Args:
+        max_delay (int): max delay
+
+    Returns:
+        task: task
+    """
+    return asyncio.create_task(wait_random(max_delay))
