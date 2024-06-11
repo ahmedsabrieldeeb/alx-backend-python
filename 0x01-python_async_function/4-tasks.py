038a787d7c3abfd52c2accb851a1b7eaee3c92af
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-11 06:54:28 +0300

    embedded task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""The basics of async"""
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Waits for a random delay between 0 and `max_delay` seconds and returns it
+
+    Args:
+        n (int): The number of times to call wait_random
+        max_delay (int): The maximum random delay
+
+    Returns:
+        ret (float): The list of all the delays
+    """
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    # avoiding sort() because it will be already sorted
+    ret = [await delay for delay in asyncio.as_completed(delays)]
+    return ret
